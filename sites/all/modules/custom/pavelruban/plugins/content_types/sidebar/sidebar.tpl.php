110896d4f1813004d1e4dd4bc3adeea1d13fa779
pavel ruban <pruban@adyax.com>
2013-03-21 11:56:48 -0600

    provide host git support

--- sites/all/modules/custom/pavelruban/plugins/content_types/sidebar/sidebar.tpl.php
+++ sites/all/modules/custom/pavelruban/plugins/content_types/sidebar/sidebar.tpl.php
@@ -5,14 +5,12 @@
  */
 ?>
 <div id="sidebar">
-  <div id="sidebar-entry">
-    <?php if (!empty($rows)): ?>
-      <h3>Recent Posts</h3>
-      <ul class="recent-posts">
-        <?php foreach($rows as $row): ?>  
-          <li><?php echo $row; ?></li>
-        <?php endforeach; ?>  
-      </ul>
-    <?php endif; ?>
-  </div>
+  <?php if (!empty($rows)): ?>
+    <h3>Recent Posts</h3>
+    <ul class="recent-posts">
+      <?php foreach($rows as $row): ?>  
+        <li><?php echo $row; ?></li>
+      <?php endforeach; ?>  
+    </ul>
+  <?php endif; ?>
 </div>
