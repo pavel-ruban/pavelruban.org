e42328627403b283a8bb7d1467a4c3adfed88b58
Pavel Ruban <pruban@adyax.com>
2016-04-02 12:52:34 +0600

    test ssl link into sidebar

--- sites/all/modules/custom/pavelruban/plugins/content_types/sidebar/sidebar.tpl.php
+++ sites/all/modules/custom/pavelruban/plugins/content_types/sidebar/sidebar.tpl.php
@@ -5,14 +5,14 @@
  */
 ?>
 <?php if (!empty($rows)): ?>
+    <h3>Get SSL CA certificate:</h3>
+    <ul>
+      <li><a href="/get-and-set-ca-ssl-certificate-for-pavelrubanorg">download CA certificate</a></li>
+    </ul>
     <h3 class="recent-posts"><span class="prefix">+</span> Recent Posts</h3>
     <ul class="recent-posts">
       <?php foreach ($rows as $row): ?>
         <li><?php echo $row; ?></li>
       <?php endforeach; ?>
-    </ul>
-    <h3>Get SSL CA certificate</h3>
-    <ul>
-      <li><a href="/get-and-set-ca-ssl-certificate-for-pavelrubanorg">download CA certificate</a></li>
     </ul>
 <?php endif; ?>
