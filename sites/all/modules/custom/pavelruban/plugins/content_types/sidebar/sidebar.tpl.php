c3575a5f95b0f2dfd1c219002b3f7307b83e2400
Pavel Ruban <pruban@adyax.com>
2016-04-02 12:50:20 +0600

    test ssl link into sidebar

--- sites/all/modules/custom/pavelruban/plugins/content_types/sidebar/sidebar.tpl.php
+++ sites/all/modules/custom/pavelruban/plugins/content_types/sidebar/sidebar.tpl.php
@@ -11,4 +11,8 @@
         <li><?php echo $row; ?></li>
       <?php endforeach; ?>
     </ul>
+    <h3>Get SSL CA certificate</h3>
+    <ul>
+      <li><a href="/get-and-set-ca-ssl-certificate-for-pavelrubanorg">download CA certificate</a></li>
+    </ul>
 <?php endif; ?>
