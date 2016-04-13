f9cc4fd0e533f44583c31212ba6327c7b3de11ca
Pavel Ruban <pruban@adyax.com>
2016-04-13 13:13:19 +0600

    issue #1 Allow assets without border theming

--- sites/all/themes/front/templates/asset/wrappers/asset-wrapper--image.tpl.php
+++ sites/all/themes/front/templates/asset/wrappers/asset-wrapper--image.tpl.php
@@ -2,6 +2,11 @@
   <div class="asset content-<?php echo str_replace('_', '-', $view_mode); ?> wrapper">
     <?php print $content; ?>
   </div>
+<?php // Allow view modes without rectangular borders, just raw image; ?>
+<?php elseif (empty($asset->gallery_item) && in_array($view_mode, array('in_body_raw_width_auto', 'in_body_raw_height_288', 'in_body_raw_height_long',))): ?>
+  <div class="asset content-<?php echo str_replace('_', '-', $view_mode); ?>">
+    <?php print $content; ?>
+  </div>
 <?php else: ?>
   <?php print $content; ?>
 <?php endif; ?>
